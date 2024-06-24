e318abe995125727068774077a4c42d2b78d69ce
ShashwatSinghal <shashwat12.18@gmail.com>
2024-06-24 16:44:52 +0530

    added comment to evenlySpaced

--- evenlySpaced/solution.java
+++ evenlySpaced/solution.java
@@ -1,5 +1,6 @@
 public class solution{
     public static boolean evenlySpaced(int a, int b, int c) {
+        //sum of smallest and largest is equal to twice of medium element
         return ((a+b==2*c)||(a+c==2*b)||(b+c==2*a));
     }
     public static void main(String args[]){
